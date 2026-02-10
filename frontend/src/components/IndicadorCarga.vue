e7329faf78a11e8a5c59c0bc1eef37b45926a376
occulta <seocculta@gmail.com>
2026-02-10 20:31:39 +0100

    Crear componente IndicadorCarga

---
+++ frontend/src/components/IndicadorCarga.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex flex-col items-center gap-2">
+    <div
+      class="size-8 rounded-full border-4 border-indigo-500 border-t-transparent animate-spin"
+    ></div>
+    <span>Cargando...</span>
+  </div>
+</template>
